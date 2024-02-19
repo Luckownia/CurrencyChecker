811fc79086f6501bf2cffe608da9310b5e1cdac5
Filip Kubala <69809370+Luckownia@users.noreply.github.com>
2024-02-19 13:53:36 +0100

    Add function fetchData
    Funciton fetchData is responsible for connect with currency exchange pages and fetch exchanges rates

--- CurrencyChecker.java
+++ CurrencyChecker.java
@@ -1,4 +1,15 @@
 public class CurrencyChecker{
+	private static Elements fetchData(String link, String way) {
+        	Elements tr = null;
+        	try {
+            		Connection connect = Jsoup.connect(link).timeout(5000);
+            		Document document = connect.get();
+            		tr = document.select(way);
+       		} catch (IOException e) {
+        		System.err.println("Błąd podczas pobierania danych z strony kantoru: " + e.getMessage());
+        	}
+        	return tr;
+	}
 
 	public static void main(String[] args) {
 	
