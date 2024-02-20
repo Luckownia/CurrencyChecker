617e78db356495afafbbf646bf4d62f0a7ff71d9
Filip Kubala <69809370+Luckownia@users.noreply.github.com>
2024-02-20 14:30:39 +0100

    Add function drawTable
    The drawTable function is responsible for rendering and displaying a table in the user interface.

--- CurrencyChecker.java
+++ CurrencyChecker.java
@@ -10,6 +10,43 @@ public class CurrencyChecker{
         	}
         	return tr;
 	}
+	private static void drawTable(Elements tr, String name) {
+            String[] columnNames = {"Waluta", "Nazwa Waluty", "Ilość", "Sprzedaż", "Kupno"};
+            DefaultTableModel model = new DefaultTableModel(null, columnNames);
+            
+        try {
+            for (int i = 0; i < tr.size(); i++) {
+                Elements td = tr.get(i).select("td");
+                if (td.text() == null) {
+                    continue;
+                }
+                if (td.size() < 1) {
+                    System.out.println("Brak kolumny z wartosciami!");
+                }
+                String[] rowData = new String[td.size()];
+                for (int j = 0; j < td.size(); j++) {
+                    if (td.get(j).text().equals("")) {
+                        rowData[j] = "0";
+                        continue;
+                    }
+                    rowData[j] = td.get(j).text();
+                }
+                model.addRow(rowData);
+            }
+
+            JFrame f;
+            JTable jt;
+            f = new JFrame();
+            f.setTitle(name);
+            jt = new JTable(model);
+            JScrollPane sp = new JScrollPane(jt);
+            f.add(sp);
+            f.pack();
+            f.setVisible(true);
+       	} catch (Exception err) {
+        	System.err.println("Something go wrong " + err.getMessage());
+        	}
+	}
 
 	public static void main(String[] args) {
 	
